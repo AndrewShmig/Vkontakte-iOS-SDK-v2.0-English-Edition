a6c785cb83182c6a52cd89ffa18b127a9a0a88ef
AndrewShmig <andrewshmig@gmail.com>
2013-10-14 10:20:44 +0400

    Updated documentation

--- Project/Vkontakte-iOS-SDK-v2.0/VKConnector/VKRequest/VKRequest.h
+++ Project/Vkontakte-iOS-SDK-v2.0/VKConnector/VKRequest/VKRequest.h
@@ -1,8 +1,5 @@
 //
-//  VKConnector.h
-//
-//  Created by Andrew Shmig on 18.12.12.
-//
+// Created by AndrewShmig on 6/28/13.
 //
 // Copyright (c) 2013 Andrew Shmig
 //
@@ -27,147 +24,273 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 //
-
 #import <Foundation/Foundation.h>
-#import "VKMethods.h"
-#import "VKModal.h"
+#import "VKCachedData.h"
 
 
-@class VKAccessToken;
-@class VKConnector;
-@class VKModal;
-@class VKStorageItem;
+/** Unknown size of the transmitted data from server
+*/
+#define NSURLResponseUnknownContentLength 0
 
-/** This protocol defines callback methods for tracking access token
- status updates. Access token is being stored in VKConnector class.
- */
-@protocol VKConnectorDelegate <NSObject>
+
+/** The prefix of the URL to which requests are made to the social network API
+*/
+static NSString *const kVKAPIURLPrefix = @"https://api.vk.com/method/";
+
+
+@class VKRequest;
+
+
+/** Protocol encapsulates the basic methods for tracking status
+of the request.
+
+The only requirement is one method - a method that returns a response, other
+are optional.
+*/
+@protocol VKRequestDelegate <NSObject>
+
+@required
+/**
+@name Required
+*/
+/** Returns the server's response in the form of Foundation object
+
+@param request request
+@param response the response from the server as a Foundation object
+*/
+- (void)VKRequest:(VKRequest *)request
+         response:(id)response;
 
 @optional
 /**
- @name Modal view
- */
-/** This callback will be executed before authorization of modal window is shown
- 
- @param connector instance of VKConnector class which will send a message
- @param view modal window
- */
-- (void)VKConnector:(VKConnector *)connector
-  willShowModalView:(VKModal *)view;
+@name Optional
+*/
+/** Is called if connection error occurred
 
-/** This callback will be executed before authorization of modal window is hidden.
- 
- @param connector instance of VKConnector class which will send a message
- @param view modal window
- */
-- (void)VKConnector:(VKConnector *)connector
-  willHideModalView:(VKModal *)view;
-
-/**
- @name Access token
- */
-/** This callback will be executed when access token expires
- 
- @param connector instance of VKConnector class which will send a message
- @param accessToken expired access token
- */
-- (void)   VKConnector:(VKConnector *)connector
-accessTokenInvalidated:(VKAccessToken *)accessToken;
-
-/** This callback will be executed after a successful access token renewal
- 
- @param connector instance of VKConnector class which will send a message
- @param accessToken renewed access token
- */
-- (void)        VKConnector:(VKConnector *)connector
-accessTokenRenewalSucceeded:(VKAccessToken *)accessToken;
-
-/** This callback will be executed if a token update was not successful.
- It is possible that internet connection was lost or user denied authorizing application
- 
- @param connector instance of VKConnector class which will send a message
- @param accessToken access token will be equal to nil
- */
-- (void)     VKConnector:(VKConnector *)connector
-accessTokenRenewalFailed:(VKAccessToken *)accessToken;
-
-/**
- @name Connection & Parsing
- */
-/** This callback will be executed if a connection problem occurs during a request
- 
- @param connector instance of VKConnector class which will send a message
- @param error object with errors description
- */
-- (void)   VKConnector:(VKConnector *)connector
+@param request request
+@param error error with a description of the cause of failure
+*/
+- (void)     VKRequest:(VKRequest *)request
 connectionErrorOccured:(NSError *)error;
 
-/** This callback will be executed if there is an invalid json response
- 
- @param connector instance of VKConnector class which will send a message
- @param error object with errors description
- */
-- (void)VKConnector:(VKConnector *)connector
+/** Is called if an error occurred parsing a server response
+
+@param request request
+@param error error with a description of the causes of failure
+*/
+- (void)  VKRequest:(VKRequest *)request
 parsingErrorOccured:(NSError *)error;
+
+/** Is called if there is an error in the response
+
+@param request request
+@param error server response with error description
+*/
+- (void)   VKRequest:(VKRequest *)request
+responseErrorOccured:(id)error;
+
+/** Is called if you want to enter captcha
+
+Additional information for handling captcha: https://github.com/AndrewShmig/Vkontakte-iOS-SDK-v2.0/issues/11
+
+@param request request
+@param captchaSid ID of a captcha
+@param captchaImage link to the image you want to show the user that he enters text from
+*/
+- (void)VKRequest:(VKRequest *)request
+       captchaSid:(NSString *)captchaSid
+     captchaImage:(NSString *)captchaImage;
+
+/** Called every time a new portion of data (method convenient
+use to display the status of loading data)
+
+@param request request
+@param totalBytes the total number of bytes to retrieve. If for some
+reasons cannot be determined, this number will be transferred to 0
+@param downloadedBytes number of loaded bytes
+*/
+- (void)VKRequest:(VKRequest *)request
+       totalBytes:(NSUInteger)totalBytes
+  downloadedBytes:(NSUInteger)downloadedBytes;
+
+/** Called every time sent a fresh batch of data (method convenient
+use to display the status of the send data, whether it uploads audio file,
+video file or document)
+
+@param request request
+@param totalBytes the total number of bytes to retrieve. If for some
+reasons cannot be determined, this number will be transferred to 0
+@param uploadedBytes number of loaded bytes
+*/
+- (void)VKRequest:(VKRequest *)request
+       totalBytes:(NSUInteger)totalBytes
+    uploadedBytes:(NSUInteger)uploadedBytes;
 
 @end
 
-/** This interface is for retrieving user profile access token and
- issuing requests for VK API using GET/POST methods
- 
- - (void)applicationDidFinishLaunching:(UIApplication *)application
- {
-    [[VKConnector sharedInstance] startWithAppID:@"YOUR_APP_ID"
-                                    permissons:@[@"friends",@"wall"]];
- }
- */
 
-@interface VKConnector : NSObject <UIWebViewDelegate, KGModalDelegate>
+/** Class to perform requests to the social network Vkontakte
+*/
+@interface VKRequest : NSObject <NSURLConnectionDataDelegate, NSCopying>
 
 /**
- @name Properties
- */
-/** VKConnector delegate
- 
- @see https://en.wikipedia.org/wiki/Delegation_pattern
- */
-@property (nonatomic, weak, readwrite) id <VKConnectorDelegate> delegate;
+@name Properties
+*/
+/** Delegate
+*/
+@property (nonatomic, weak, readwrite) id <VKRequestDelegate> delegate;
 
-/** VKontakte application identifier generated during registration process
- */
-@property (nonatomic, strong, readonly) NSString *appID;
+/** Arbitrary signature of the request. Enables you to identify the request if
+one delegate performs the process of multiple requests.
+*/
+@property (nonatomic, strong, readwrite) id signature;
 
-/** Permissions list
- */
-@property (nonatomic, strong, readonly) NSArray *permissions;
+/** Cache lifetime of the current request. By default, the cache lifetime is one hour.
+*/
+@property (nonatomic, assign, readwrite) VKCachedDataLiveTime cacheLiveTime;
 
-/**
- @name Methods
- */
-/** Class methods for retrieving singleton instance
- * If object is missing it will be created.
- */
-+ (id)sharedInstance;
+/** Offline request mode. In this mode, the data will be requested from the cache and returned
+even in the case of expiration (removal will not happen).
+The default mode is turned off.
+*/
+@property (nonatomic, assign, readwrite) BOOL offlineMode;
 
 /**
- @name User authorization for application
- */
-/** Initializes connection with passed parameters
- 
- @param appID VKontakte application identifier generated during registration process
- @param permissions Array with permission flags that the application needs to request
- */
-- (void)startWithAppID:(NSString *)appID
-            permissons:(NSArray *)permissions;
+@name Class methods
+*/
+/** Creates and returns a request
+
+@param request request that will be used as the basis
+@param delegate delegate to receive notifications/messages
+the state of an object and data
+
+@return an instance of the class VKRequest
+*/
++ (instancetype)request:(NSURLRequest *)request
+               delegate:(id <VKRequestDelegate>)delegate;
+
+/** Creates and returns a request
+
+@param httpMethod GET or POST
+@param url the URL that will be implemented request
+@param headers request headers
+@param body the body of the request
+@param delegate delegate to receive notifications/messages
+the state of an object and data
+
+@return an instance of the class VKRequest
+*/
++ (instancetype)requestHTTPMethod:(NSString *)httpMethod
+                              URL:(NSURL *)url
+                          headers:(NSDictionary *)headers
+                             body:(NSData *)body
+                         delegate:(id <VKRequestDelegate>)delegate;
+
+/** Creates and returns a request
+
+@param methodName the name of the API method (users.get, groups.join etc)
+@param options dictionary of parameters to be passed to this method
+@param delegate delegate to receive notifications/messages
+the state of an object and data
+
+@return an instance of the class VKRequest
+*/
++ (instancetype)requestMethod:(NSString *)methodName
+                      options:(NSDictionary *)options
+                     delegate:(id <VKRequestDelegate>)delegate;
 
 /**
- @name Authorization cookies management methods
- */
-/** Removes cookies that are received during the authorization process of the last application user.
- Might be needed if the user wants to use different profiles or for authorization of a different person.
- 
- tip: UIWebView saves authorization cookies
- */
-- (void)clearCookies;
+@name Instance methods
+*/
+/** The main methods of object initialization
 
+@param request the request that will be used as the basis
+
+@return an object of type VKRequest
+*/
+- (instancetype)initWithRequest:(NSURLRequest *)request;
+
+/** The init method of the object
+
+@param httpMethod GET or POST
+@param url the URL that will be implemented request
+@param headers request headers
+@param body the body of the request
+
+@return object type VKRequest
+*/
+- (instancetype)initWithHTTPMethod:(NSString *)httpMethod
+                               URL:(NSURL *)url
+                           headers:(NSDictionary *)headers
+                              body:(NSData *)body;
+
+/** The init method of the object
+
+Consider this example:
+
+VKRequest *request = [[VKRequest alloc] initWithMethod:@"users.get"
+options:@{@"fields": @"nickname,bdate,status"}];
+
+Object will be created VKRequest and subsequently implemented the method call users.get
+the social network. Option fields will be equal to "nickname,bdate,status" and therefore
+social network will return the nickname, date of birth and the status of the current user.
+
+@param methodName names API method (users.get, groups.join etc)
+@param options dictionary of parameters to be passed to this method
+
+@return object class VKRequest
+*/
+- (instancetype)initWithMethod:(NSString *)methodName
+                       options:(NSDictionary *)options;
+
+/** Start request
+*/
+- (void)start;
+
+/** Cancel request
+*/
+- (void)cancel;
+
+/**
+@name Add files to the body of the request
+*/
+/** Adding audio file data to the contents of the HTTP request body
+
+@param file byte representation of an audio file
+@param name the name of the audio file
+@param field names the HTML field that was used to send a file
+*/
+- (void)appendAudioFile:(NSData *)file
+                   name:(NSString *)name
+                  field:(NSString *)field;
+
+/** Adding video file data to the contents of the HTTP request body
+
+@param file byte representation of a video file
+@param name the name of the video file
+@param field name of an HTML field which was used to send a file
+*/
+- (void)appendVideoFile:(NSData *)file
+                   name:(NSString *)name
+                  field:(NSString *)field;
+
+/** Adding document data file the contents of the HTTP request body
+
+@param file byte representation of the document
+@param name the name of the document file
+@param field name of an HTML field which was used to send a file
+*/
+- (void)appendDocumentFile:(NSData *)file
+                      name:(NSString *)name
+                     field:(NSString *)field;
+
+/** Add the image data to the contents of the HTTP request body
+
+@param file byte representation of the image
+@param name the name of the image file
+@param field name of an HTML field which was used to send a file
+*/
+- (void)appendImageFile:(NSData *)file
+                   name:(NSString *)name
+                  field:(NSString *)field;
 @end
